83d55955281b01ab9da97892a2cbece69ca25f87
Shushushushin <116691632+Shushushushin@users.noreply.github.com>
2022-10-29 11:27:16 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+//求1*2*3*4……
+/*
+#include<stdio.h>
+
+int mul (int a)
+{
+    int i;
+    int t=1;
+    for(i=1 ; i<=a; i++)
+        t=t*i;
+    return t;
+}
+int main()
+{
+    int a;
+    printf("请输入a的值：");
+    scanf("%d",&a);
+    int t;
+    t=mul(a);
+    printf("结果为：%d\n",t);
+    return 0;
+}
+
+
+void mul (int a)
+{
+    int i;
+    int t=1;
+    for(i=1 ; i<=a; i++)
+        t=t*i;
+    printf("结果为：%d\n",t);
+}
+int main()
+{
+    int a;
+    printf("请输入a的值：");
+    scanf("%d",&a);
+    int t;
+    mul(a);
+    return 0;
+}
+*/
+
+
